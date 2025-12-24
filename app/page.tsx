e149b47cacf89fdf672975fdf51d663895f6ae9e
KrisD23 <krishandwive9@gmail.com>
2025-12-24 14:16:17 +0530

    create and add cta section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Cta from "@/components/Insights-page/Cta";
 import Hero from "@/components/Insights-page/Hero";
 
 const Home = () => {
   return (
     <main>
       <Hero />
+      <Cta />
     </main>
   );
 };
