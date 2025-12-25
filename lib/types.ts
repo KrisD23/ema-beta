e58cfcb748dc746e19487e6479dbdfcd87895805
KrisD23 <krishandwive9@gmail.com>
2025-12-25 17:29:33 +0530

    add types for contentful

--- lib/types.ts
+++ lib/types.ts
@@ -18,3 +18,39 @@ export type SocialLink = {
   url: string;
   icon: string;
 };
+
+import { EntrySkeletonType, Asset } from "contentful";
+
+export interface HeroFields {
+  heroBatch: string;
+  heading: string;
+  description: string;
+  animation: Asset;
+}
+
+export interface HeroSkeleton extends EntrySkeletonType {
+  contentTypeId: "hero";
+  fields: HeroFields;
+}
+
+export interface SolutionCardsFields {
+  cardImage: Asset;
+  cardHeading: string;
+  cardDescription: string;
+}
+
+export interface SolutionCardsSkeleton extends EntrySkeletonType {
+  contentTypeId: "solutionCards";
+  fields: SolutionCardsFields;
+}
+
+export interface FeatureShowcaseCardsFeilds {
+  cardImage: Asset;
+  heading: string;
+  description: string;
+}
+
+export interface FeatureShowcaseCardsSkeleton extends EntrySkeletonType {
+  contentTypeId: "featureShowcaseCards";
+  fields: FeatureShowcaseCardsFeilds;
+}
